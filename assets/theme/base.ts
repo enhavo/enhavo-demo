91b998b165f42634d2b0b3dde3bc521947f0fdda
indivisualvj <account@bennson.de>
2020-10-16 10:54:10 +0200

    disable modules not working better content

--- assets/theme/base.ts
+++ assets/theme/base.ts
@@ -1,9 +1,3 @@
-// import "@enhavo/theme/init"
-// import "@enhavo/block/init"
-// import "@enhavo/slider/init"
-// import "@enhavo/shop/init"
-// import "@enhavo/article/init"
-// import "@enhavo/calendar/init"
 import "./styles/style.scss"
 
 import Theme from "./lib/Theme";
