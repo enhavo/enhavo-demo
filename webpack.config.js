ccceaf80cc098f187e3c3483058cea5e2e977438
jmiras <jmiras@iMac-von-jmiras.local>
2024-03-29 13:41:56 +0800

    style demo layout

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ EnhavoEncore.add(
     [ new AppThemePackage() ],
     Encore => {
         Encore
-            .addEntry('base', './assets/theme/base')
+            .addEntry('base', './assets/theme/entrypoints/base')
 
     },
     config => {}
