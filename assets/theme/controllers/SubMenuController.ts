73228abb30d58fef3aca7abd0862a7d66c96f863
jmiras <jm@weareindeed.com>
2024-08-27 18:42:17 +0800

    refactor css to scss

--- assets/theme/controllers/SubMenuController.ts
+++ assets/theme/controllers/SubMenuController.ts
@@ -1,7 +1,33 @@
 import { Controller } from "@hotwired/stimulus";
 import * as $ from 'jquery';
-export default class extends Controller{
-
+export default class extends Controller {
+    static targets = ['trigger', 'menu'];
     connect() {
+        this.setupHover();
+        console.log('submenu working');
+    }
+
+    setupHover() {
+        const _this = this;
+
+        // Hover on the headline to show the submenu
+        $(this.triggerTargets).hover(
+            function () {
+                $(this).siblings(_this.menuTarget).addClass('active');
+            },
+            function () {
+                $(this).siblings(_this.menuTarget).removeClass('active');
+            }
+        );
+
+        // Keep submenu visible on hover
+        $(this.menuTargets).hover(
+            function () {
+                $(this).addClass('active');
+            },
+            function () {
+                $(this).removeClass('active');
+            }
+        );
     }
 }
