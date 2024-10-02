90b0473423c93530013703f2ff207af99af49b95
jmiras <jm@weareindeed.com>
2024-10-02 16:39:04 +0800

    refactored blocks

--- assets/theme/controllers/SwiperTeaserController.ts
+++ assets/theme/controllers/SwiperTeaserController.ts
@@ -6,11 +6,13 @@ export default class extends Controller {
     static targets = ["slide", "navButton"];
 
     connect() {
-        console.log('Slick carousel initialized', this.element);
         $(this.slideTarget).slick({
-            slidesToShow: 4,
+            slidesToShow: 2,
+            centerMode:true,
             slidesToScroll: 1,
             autoplay: true,
+            arrows:false,
+            dots: true,
             autoplaySpeed: 2000,
             pauseOnHover:true,
             responsive: [
@@ -18,6 +20,7 @@ export default class extends Controller {
                     breakpoint: 780,
                     settings: {
                         slidesToShow: 2,
+                        centerMode:true,
                         slidesToScroll: 1,
                     }
                 },
@@ -30,27 +33,5 @@ export default class extends Controller {
                 }
             ]
         });
-
-        this.setupNavigation();
-
-        $(this.slideTarget).on('afterChange', (event, slick, currentSlide) => {
-            this.highlightActiveButton(currentSlide);
-        });
-    }
-
-    setupNavigation() {
-        this.navButtonTargets.forEach((button) => {
-            const index = button.dataset.index;
-
-            button.addEventListener('click', () => {
-                $(this.slideTarget).slick('slickGoTo', index);
-                this.highlightActiveButton(index);
-            });
-        });
-    }
-
-    highlightActiveButton(index) {
-        this.navButtonTargets.forEach((btn) => btn.classList.remove('active'));
-        this.navButtonTargets[index].classList.add('active');
     }
 }
